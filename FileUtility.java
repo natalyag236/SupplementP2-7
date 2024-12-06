c909d8230f405b2f8a487e695980fa32caa80cde
Natalya Goldsmith <ngol1755@students.vsu.edu>
2024-12-06 18:55:59 -0500

    doc: add documentation to createAndSaveFile

--- FileUtility.java
+++ FileUtility.java
@@ -1,6 +1,14 @@
 import java.io.*;
 
 public class FileUtility{
+     /**
+     * Creates a file with the specified name, writes the provided content to it, and saves it.
+     *
+     * @param filename the name of the file to create (including path if needed)
+     * @param content  the content to write into the file
+     * @return the created File object
+     * @throws IOException if an I/O error occurs during file creation or writing
+     */
     public static File createandSaveFile(String filename, String content) throws IOException{
         File file = new File(filename);
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))){
