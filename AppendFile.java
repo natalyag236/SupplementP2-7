c2333577c700bba7ac0a4f88e977e85f76f0a396
Natalya Goldsmith <ngol1755@students.vsu.edu>
2024-12-06 20:23:22 -0500

    doc: add documentation for read , create, save and append

--- AppendFile.java
+++ AppendFile.java
@@ -2,7 +2,16 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
 
+
 public class AppendFile {
+
+    /**
+     * Reads the content of a file and returns it as a string.
+     *
+     * @param filename the name of the file to be read
+     * @return the content of the file as a string
+     * @throws IOException if an I/O error occurs or the file does not exist
+     */
     public static String readFileContent(String filename) throws IOException {
         Path filePath = Path.of(filename);
 
@@ -12,6 +21,14 @@ public class AppendFile {
 
         return Files.readString(filePath, StandardCharsets.UTF_8);
     }
+
+    /**
+     * Creates and saves a file with the given content. If the file already exists, it overwrites the content.
+     *
+     * @param filename the name of the file to be created
+     * @param content  the content to be written to the file
+     * @throws IOException if an I/O error occurs
+     */
     public static void createAndSaveFile(String filename, String content) throws IOException {
         Path filePath = Path.of(filename);
 
@@ -21,6 +38,14 @@ public class AppendFile {
 
         Files.writeString(filePath, content, StandardCharsets.UTF_8);
     }
+
+    /**
+     * Appends the given content to an existing file. If the file does not exist, throws an IOException.
+     *
+     * @param filename the name of the file to append to
+     * @param content  the content to append to the file
+     * @throws IOException if an I/O error occurs or the file does not exist
+     */
     public static void appendToFile(String filename, String content) throws IOException {
         Path filePath = Path.of(filename);
 
